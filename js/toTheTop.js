46ddc0faca96d44ea3fffa153cc8b8e7ddefd871
bespvlevnost <107690520+bespvlevnost@users.noreply.github.com>
2022-10-14 01:50:26 +0300

    Add files via upload

--- js/toTheTop.js
+++ js/toTheTop.js
@@ -3,8 +3,10 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
         document.getElementById("myBtn").style.display = "block";
+        document.getElementById("hidingHeader").style.display = "none";
     } else {
         document.getElementById("myBtn").style.display = "none";
+        document.getElementById("hidingHeader").style.display = "flex";
     }
 }
 
